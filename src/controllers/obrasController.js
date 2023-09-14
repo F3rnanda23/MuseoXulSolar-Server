7a704d8d2e1789ec52a0c2f30b4d02c3ee774653
FrancoNahuelPerez <francarp001@gmail.com>
2023-09-14 15:49:34 -0300

    commit de prueba

---
+++ src/controllers/obrasController.js
@@ -0,0 +1,44 @@
+const { Obras } = require ("../db");
+
+const getObras = async () => {
+    try {
+        const obras = await Obras.findAll();
+        return obras;
+    } catch (error) {
+        console.log("Error en getObras:", error.message);
+    }
+};
+const getObrasId = async (idObra) => {
+    try {
+        const obra = await Obras.findByPk(idObra);
+        return obra;
+    } catch (error) {
+        console.log("Error en getObrasId:", error.message);
+    }
+};
+const postObras = async (bodyObra) => {
+    try {
+        let obraCreada = await Obras.findOrCreate({
+            where: {name: bodyObra.name },
+            defaults: {
+                year: bodyObra.year,
+                name: bodyObra.name,
+                image: bodyObra.image,
+                usuarioXObras: bodyObra.usuarioId
+            },
+        }).then (([obra, creado])=> {
+            if(creado){
+                return obra;
+            } else {
+                return;
+            }
+        });
+        return obraCreada
+    } catch (error) {
+        console.log("Error en postObras:", error.message);
+    }
+};
+
+module.exports = {
+    getObras, getObrasId, postObras,
+};
