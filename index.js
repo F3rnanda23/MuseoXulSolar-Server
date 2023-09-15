a3c0ee1c251ee5ba37fbb4697a98104f81dc2d5b
joaquin-espinosa733 <juaco.enero9@gmail.com>
2023-09-14 22:26:12 -0300

    tabla usuario

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const server = require('./src/app.js');
+const server = require('./src/index.js');
 const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 
-const { NODE_ENV } = process.env;
+// const { NODE_ENV } = process.env;
 
 // En desarrollo, usa force: true para sincronizar la base de datos
-const forceSync = NODE_ENV === 'development';
+// const forceSync = NODE_ENV === 'development';
 
-conn.sync({ force: forceSync }).then(() => {
-  server.listen(3001, () => {
+conn.sync({ alter: true }).then(() => {
+  server.listen(port, () => {
     console.log(`%s listening at 3001`); // eslint-disable-line no-console
   });
 });
