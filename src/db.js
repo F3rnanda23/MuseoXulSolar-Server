311d670a293ac90cc8878a16258f59bebe7d7121
joaquin-espinosa733 <juaco.enero9@gmail.com>
2023-09-15 01:30:51 -0300

    pgs

--- src/db.js
+++ src/db.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+const pg = require("pg")
 const {
   DB_USER, DB_PASSWORD, DB_HOST,DB_DEPLOY
 } = process.env;
@@ -9,6 +10,7 @@ const {
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectModule: pg,
 });
 const basename = path.basename(__filename);
 
