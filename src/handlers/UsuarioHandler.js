c047a2e5b37512ee58b550a72d976299c4b4545f
maxirivero <maximilianoa.castillorivero@gmail.com>
2023-09-22 01:00:33 -0300

    nodemailer funcionando

--- src/handlers/UsuarioHandler.js
+++ src/handlers/UsuarioHandler.js
@@ -6,7 +6,7 @@ const {
     editUser,
     loginUser
 } = require("../controllers/UsuarioController.js");
-
+const {sendEmail} = require("../nodemailer/nodemailer.js")
 
 
 const bringUsers = async (req, res) => {
@@ -23,6 +23,7 @@ const loginUserHandler = async (req, res) => {
     const { birthday, name, email, phone, password, admin } = req.body;
     try {
         const postUser = await createUsuario({ birthday, name, email, phone, password, admin })
+        sendEmail(postUser.email)
         res.status(200).json(postUser);
     } catch (error) {
         res.status(404).json({ error: error.message })
