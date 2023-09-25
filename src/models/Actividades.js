19d1685d9ea8c026f85d57d3326ea6e7a43c4e38
maxirivero <maximilianoa.castillorivero@gmail.com>
2023-09-24 22:02:01 -0300

    actividades/hora/string

--- src/models/Actividades.js
+++ src/models/Actividades.js
@@ -27,7 +27,7 @@ module.exports = (sequelize) => {
         allowNull: false
       },
       hora: {
-        type: DataTypes.TIME,
+        type: DataTypes.STRING,
         allowNull: false
       },
     },
