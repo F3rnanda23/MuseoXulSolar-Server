5568199d63afb5c721abd7ef1626d2502b324532
diegoalparra <diegoalparra08@gmail.com>
2023-09-26 18:13:37 -0500

    commentarios funcionan

--- src/handlers/ComentariosHandler.js
+++ src/handlers/ComentariosHandler.js
@@ -2,10 +2,11 @@ const {postComentarios,
 getComentarios} = require("../controllers/ComentariosControllers")
 
 const postComentariosHandler = async(req,res) =>{
-    console.log(req.body);
+   
     const {description, date, UsuarioId, rating, email} = req.body
     try {
         const commentPosteado = await postComentarios({description,email, date, UsuarioId, rating})
+        console.log(commentPosteado);
         res.status(200).json(commentPosteado)
     } catch (error) {
         res.status(400).json({error:error.message})
