8c40237fb1c97f519150bc7b543cae71072dd06c
joaquin-espinosa733 <juaco.enero9@gmail.com>
2023-09-30 13:04:17 -0300

    tab

--- src/routes/ActivityRoute.js
+++ src/routes/ActivityRoute.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { 
+const {
     createActivityHandler,
     allActivityHandler,
     putActivityHandler,
@@ -7,7 +7,7 @@ const {
     restoreActivity,
     idActHandler
 } = require("../handlers/ActivityHandlers.js");
-const {Actividades,Usuario} = require("../db.js");
+const { Actividades, Usuario } = require("../db.js");
 
 const routerActivity = Router();
 
@@ -21,26 +21,26 @@ routerActivity.put("/restaurar/:id", restoreActivity);
 // Ruta para crear una reserva
 routerActivity.post("/reservar", async (req, res) => {
     try {
-      const { usuarioId, actividadId } = req.body;
-  
-      // Verifica si el usuario y la actividad existen antes de crear la reserva
-      const usuario = await Usuario.findByPk(usuarioId);
-      const actividad = await Actividades.findByPk(actividadId);
-  
-      if (!usuario || !actividad) {
-        return res.status(404).json({ message: "Usuario o actividad no encontrado." });
-      }
-  
-      // Utiliza el método setUsuario para establecer la relación
-      await actividad.setUsuarios(usuario);
-  
-      return res.status(200).json({ message: "Reserva creada con éxito." });
+        const { usuarioId, actividadId } = req.body;
+
+        // Verifica si el usuario y la actividad existen antes de crear la reserva
+        const usuario = await Usuario.findByPk(usuarioId);
+        const actividad = await Actividades.findByPk(actividadId);
+
+        if (!usuario || !actividad) {
+            return res.status(404).json({ message: "Usuario o actividad no encontrado." });
+        }
+
+        // Utiliza el método setUsuario para establecer la relación
+        await actividad.setUsuarios(usuario);
+
+        return res.status(200).json({ message: "Reserva creada con éxito." });
     } catch (error) {
-      console.error("Error al crear la reserva:", error);
-      return res.status(500).json({ message: "Error interno del servidor." });
+        console.error("Error al crear la reserva:", error);
+        return res.status(500).json({ message: "Error interno del servidor." });
     }
-  });
-  
+});
+
 
 
 module.exports = routerActivity;
