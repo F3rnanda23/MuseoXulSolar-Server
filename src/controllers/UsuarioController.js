1cb46d78c600f4c53b4bef2527086ae24a23d6d4
joaquin-espinosa733 <juaco.enero9@gmail.com>
2023-10-02 19:48:30 -0300

    alertadeM...

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -142,6 +142,11 @@ const buscarEmailConGoolge = async (email) => {
     return userInfo;
 }
 
+const buscarEmaiBloqueado = async(email)=>{
+    const response = await Usuario.findOne({where: {email}});
+    return response;
+}
+
 module.exports = {
     createUsuario,
     deleteLogicUser,
@@ -151,5 +156,6 @@ module.exports = {
     loginUser,
     buscarUsuarioPorEmail,
     buscarEmailConGoolge,
-    idUser
+    idUser,
+    buscarEmaiBloqueado
 }
