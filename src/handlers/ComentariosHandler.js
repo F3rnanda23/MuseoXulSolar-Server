e1cb5ada83c06813c6fd77cf2bd8c19d3c747399
diegoalparra <diegoalparra08@gmail.com>
2023-10-03 21:09:32 -0500

    me lleva

--- src/handlers/ComentariosHandler.js
+++ src/handlers/ComentariosHandler.js
@@ -8,9 +8,9 @@ const postComentariosHandler = async(req,res) =>{
     try {
         const commentPosteado = await postComentarios({description, email, date, UsuarioId, rating})
         console.log(commentPosteado);
-        res.status(200).json(commentPosteado);
+       return res.status(200).json(commentPosteado);
     } catch (error) {
-        res.status(400).json({error:error.message})
+       return res.status(400).json({error:error.message})
     }
 }
 
